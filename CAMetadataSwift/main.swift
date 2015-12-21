7360b419aadc82100b0c3a5b14bfd478d7759d82
Jeff Vautin <jeff@jeffvautin.com>
2015-12-20 22:13:32 -0500

    Initial Commit

---
+++ CAMetadataSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CAMetadataSwift
+//
+//  Created by Jeff Vautin on 12/20/15.
+//  Copyright © 2015 Jeff Vautin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
