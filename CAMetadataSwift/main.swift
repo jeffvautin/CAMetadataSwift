a72c8c2a311da4949d15359dded9634269f866b2
Jeff Vautin <jeff@jeffvautin.com>
2015-12-21 23:55:25 -0500

    Finished code in Swift with help of this: https://github.com/victor/Learning-Core-Audio-in-Swift/blob/master/CAMetadata/CAMetadata/main.swift

--- CAMetadataSwift/main.swift
+++ CAMetadataSwift/main.swift
@@ -7,6 +7,37 @@
 //
 
 import Foundation
+import AudioToolbox
 
-print("Hello, World!")
+if (Process.argc < 2) {
+    print("Usage: CAMetadata /full/path/to/audiofile\n")
+    exit(1)
+}
 
+let audioFilePath = (Process.arguments[1] as NSString).stringByExpandingTildeInPath
+let audioURL = NSURL.fileURLWithPath(audioFilePath)
+
+var audioFile: AudioFileID = nil
+var theErr = noErr
+
+theErr = AudioFileOpenURL(audioURL, AudioFilePermissions.ReadPermission, 0, &audioFile)
+
+assert(theErr == noErr)
+
+var dictionarySize: UInt32 = 0
+var isWriteable: UInt32 = 0
+
+theErr = AudioFileGetPropertyInfo(audioFile, kAudioFilePropertyInfoDictionary, &dictionarySize, &isWriteable)
+
+assert(theErr == noErr)
+
+var dictionary: CFDictionaryRef? = nil
+theErr = AudioFileGetProperty(audioFile, kAudioFilePropertyInfoDictionary, &dictionarySize, &dictionary)
+
+assert(theErr == noErr)
+print("dictionary: \(dictionary)")
+
+theErr = AudioFileClose(audioFile)
+assert(theErr == noErr)
+
+exit(0)
